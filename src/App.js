e77398f271cb45a90c75b5c0ceedc9e5c7dabfab
rishavtalukdar <rishavtalukdar1992@gmail.com>
2024-06-06 21:04:31 +0530

    fixed failed test cases

--- src/App.js
+++ src/App.js
@@ -5,34 +5,35 @@ function App() {
   const [time, settime] =useState(0)
   const [isRunning, setIsRunning] =useState(false)
 
+//  Generally speaking, using setState inside useEffect will create an infinite loop that most likely you don't want to cause. 
+// There are a couple of exceptions to that rule which I will get into later.
   useEffect (()=>{
-    let interval
+    let intervalId
     if(isRunning){
-      interval =setInterval(() => {
-        settime(time + 1)
-      }, 1000);
+      intervalId =setInterval(()=>{
+        settime (time+1)
+      },100)
     }
-   return ()=>clearInterval(interval)
-  },[isRunning ,time])
+    return()=>clearInterval(intervalId)
+  },[time,isRunning])
 
-  const startAndStop = () => {
-    setIsRunning(!isRunning);
-  };
 
-  const reset =()=>{
-    settime(0)
+  const startAndStop =() =>{
+    setIsRunning (!isRunning)
+  }
+  const reset =() =>{
+    settime (0)
   }
 
-  const minute =Math.floor(time/60)
-  const second =Math.floor(time%60)
+  const minute = Math.floor(time/60)
+  const second = Math.floor(time%60)
 
   return (
     <div>
       <h1>Stopwatch</h1>
-      <h3>Time: {minute.toString().padStart(2, "0")}: {second.toString().padStart(2, "0")}</h3>
+      <h3>Time: {minute.toString()}:{second.toString().padStart(2,"0")} </h3>
       <div>
-        <button onClick={startAndStop}>
-          {isRunning?"Stop":"Start"}</button>
+        <button onClick={startAndStop}>Start</button>
         <button onClick={reset}>Reset</button>
       </div>
     </div>
